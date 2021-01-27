f3be4ec0419def51cb5c8837b319afb7a35cfdf5
seaw7777 <72549589+seaw7777@users.noreply.github.com>
2021-01-27 17:49:51 +0900

    파일업로드 구현 , 이미지 불러오기 필요

--- Backend/src/main/java/com/web/dazu/controller/FileUploadController.java
+++ Backend/src/main/java/com/web/dazu/controller/FileUploadController.java
@@ -1,20 +1,15 @@
 package com.web.dazu.controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile; 
-
+import javax.swing.filechooser.FileSystemView; 
 import java.io.File; 
 
-@RestController
-@RequestMapping("/fileupload")
 public class FileuploadController {
-	@PostMapping
-	public String uploadSingle(@RequestParam("files") MultipartFile file,
-			@RequestParam("address") String address,@RequestParam("detailaddress") String detailaddress) throws Exception {
-	    String rootPath = "C:\\Image";
-	    String basePath = rootPath + "/" + "store";
+	@PostMapping("/single")
+	public String uploadSingle(@RequestParam("files") MultipartFile file) throws Exception {
+	    String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
+	    String basePath = rootPath + "/" + "single";
 	    String filePath = basePath + "/" + file.getOriginalFilename();
 	    File dest = new File(filePath);
 	    file.transferTo(dest); // 파일 업로드 작업 수행
