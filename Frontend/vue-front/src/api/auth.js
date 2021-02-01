0db31942a494621c2e20bdecffd8d536eced940e
정예림 <jinju151002@gmail.com>
2021-02-01 17:40:50 +0900

    feat:고객님 추가입력 기본 form 보내기

--- Frontend/vue-front/src/api/auth.js
+++ Frontend/vue-front/src/api/auth.js
@@ -4,5 +4,8 @@ import { instance } from './index';
 function logoutUser(accessToken) {
   return instance.get(`logout/${accessToken}`);
 }
-
-export { logoutUser };
+// 고객님 추가 정보 API
+function customerInfoAPI(customerInfo) {
+  return instance.post('member/insertMember', customerInfo);
+}
+export { logoutUser, customerInfoAPI };
--- Frontend/vue-front/src/components/accounts/CustomerStepForm.vue
+++ Frontend/vue-front/src/components/accounts/CustomerStepForm.vue
@@ -8,19 +8,19 @@
             <v-text-field
               v-model="address"
               :counter="10"
-              label="거주 중인 주소를 입력해주세요."
+              label="거주중인 도로명 주소를 입력해주세요."
               required
             ></v-text-field>
           </v-col>
 
           <v-col cols="12">
             <v-text-field
-              v-model="detailaddress"
+              v-model="detailAddress"
               :counter="10"
-              label="상세 주소를 입력해주세요"
+              label="상세주소를 입력해주세요."
               required
             ></v-text-field>
-            <v-btn class="ma-2" outlined color="indigo">
+            <v-btn class="ma-2" outlined color="indigo" @click="submitInfo">
               NEXT
             </v-btn>
           </v-col>
@@ -31,7 +31,36 @@
 </template>
 
 <script>
-export default {};
+import { customerInfoAPI } from '@/api/auth';
+export default {
+  data() {
+    return {
+      address: '',
+      detailAddress: '',
+      usertype: 1,
+    };
+  },
+  methods: {
+    async submitInfo() {
+      try {
+        console.log(this.$store.state.token);
+        const response = await customerInfoAPI({
+          // accessToken: this.$store.state.token,
+          address: this.address,
+          address_detail: this.detailAddress,
+          // nickname: '',
+          // profileImage: '',
+          usercode: '123444',
+          usertype: this.usertype,
+        });
+        this.$router.push('/main');
+        console.log(response);
+      } catch (error) {
+        console.log('error');
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
