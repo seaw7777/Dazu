e6295e824dadaf0c34282839ac34c0a5d3e870e1
김지은 <vldzmf0805@naver.com>
2021-02-02 11:23:30 +0900

    env

--- Backend/src/main/java/com/web/dazu/controller/NoticeController.java
+++ Backend/src/main/java/com/web/dazu/controller/NoticeController.java
@@ -6,11 +6,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.web.dazu.model.Member;
 import com.web.dazu.model.Notice;
 import com.web.dazu.service.NoticeService;
+
+import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RequestMapping("/notice")
@@ -20,6 +25,7 @@ public class NoticeController {
 	@Autowired
 	private NoticeService service;
 	
+	@ApiOperation(value = "등록된 모든 공지사항을 읽는다.", response = List.class)
 	@GetMapping("/list")
 	public List<Notice> selectAllNotice() {
 		List<Notice> list = new ArrayList<Notice>();
@@ -30,4 +36,14 @@ public class NoticeController {
 		}
 		return list;
 	}	
+	
+	@ApiOperation(value = "새로운 공지사항을 등록한다.")
+	@PostMapping("/insert")
+	public void insertNotice(@RequestBody Notice notice) {
+		try {
+			service.insertNotice(notice);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Backend/src/main/java/com/web/dazu/service/StoreService.java
+++ Backend/src/main/java/com/web/dazu/service/StoreService.java
@@ -8,4 +8,6 @@ public interface StoreService {
 
 	List<Store> selectAllStore(String dong) throws Exception;
 
+	void insertStore(Store store) throws Exception;
+
 }
