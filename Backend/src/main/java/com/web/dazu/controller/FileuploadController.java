104c6ab1fd194cc7707ff678495a2dfae0f1cfd3
seaw7777 <72549589+seaw7777@users.noreply.github.com>
2021-02-02 11:43:15 +0900

    S04P13D104-24 aws 인스턴스 패스 설정

--- Backend/src/main/java/com/web/dazu/controller/FileuploadController.java
+++ Backend/src/main/java/com/web/dazu/controller/FileuploadController.java
@@ -1,4 +1,5 @@
 package com.web.dazu.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.web.dazu.service.FileUploadService;
 
 import io.swagger.annotations.ApiOperation;
 
@@ -17,20 +20,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/fileupload")
 public class FileuploadController {
-	
+	@Autowired
+	private FileUploadService fileuploadservice; 
 	@ApiOperation(value = "파일 업로드 및 회원 정보 받아오기", response = String.class)
 	@PostMapping
 	public String uploadSingle(@RequestPart(value = "files", required = true) List<MultipartFile> file,
 			@RequestParam("address") String address,@RequestParam("detailaddress") String detailaddress) throws Exception {
 		System.out.println("진입");
 		for (int i = 0; i < file.size(); i++) {
-		    String rootPath = "C:\\Image";
+		    String rootPath = "home/Image";
 		    String basePath = rootPath + "/" + "store";
 		    String filePath = basePath + "/" + file.get(i).getOriginalFilename();
 		    File dest = new File(filePath);
+		    fileuploadservice.fileup(filePath);
 		    file.get(i).transferTo(dest); // 파일 업로드 작업 수행
 		}
-
 	    return "uploaded";
 	}
 }
