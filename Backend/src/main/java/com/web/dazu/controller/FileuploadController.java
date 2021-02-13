60eb3198d95aacd2d59ea6ba5dc9abf3060bcc66
seaw7777 <72549589+seaw7777@users.noreply.github.com>
2021-02-14 04:12:06 +0900

    파일 불러오기 구현중

--- Backend/src/main/java/com/web/dazu/controller/FileuploadController.java
+++ Backend/src/main/java/com/web/dazu/controller/FileuploadController.java
@@ -1,44 +1,35 @@
 package com.web.dazu.controller;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.web.dazu.model.dummy;
 import com.web.dazu.service.FileUploadService;
-
 import io.swagger.annotations.ApiOperation;
-
-import java.io.File;
-import java.util.List; 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RestController
 @RequestMapping("/file")
 public class FileuploadController {
 	@Autowired
-	private FileUploadService fileuploadservice; 
-	@ApiOperation(value = "파일 업로드 및 회원 정보 받아오기", response = String.class)
-	//@RequestParam("address") String address,@RequestParam("detailaddress") String detailaddress
-	//@RequestPart(value = "key", required = false) dummy dum
-	@PostMapping("/fileupload")
-	public String uploadSingle(@RequestPart(value = "files", required = true) List<MultipartFile> file
-			) throws Exception {
-		System.out.println("진입");
-		System.out.println(file.get(0).getOriginalFilename());
+	private FileUploadService fileuploadservice;
 
-//		for (int i = 0; i < file.size(); i++) {
-//		    String rootPath = "home/Image/";
-//		    String basePath = rootPath + "/" + "store";
-//		    String filePath = basePath + "/" + file.get(i).getOriginalFilename();
-//		    File dest = new File(filePath);
-//		    fileuploadservice.fileup(filePath);
-//		    file.get(i).transferTo(dest); // 파일 업로드 작업 수행
-//		}
-	    return "uploaded";
+	@ApiOperation(value = "feed image 조회 ", notes = "feed Image를 반환합니다. 못찾은경우 기본 image를 반환합니다.")
+	@GetMapping(value = "image/{imagename}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<byte[]> userSearch(@PathVariable("imagename") String imagename) throws IOException {
+		InputStream imageStream = new FileInputStream("home/Image" + imagename);
+		byte[] imageByteArray = IOUtils.toByteArray(imageStream);
+		imageStream.close();
+		return new ResponseEntity<byte[]>(imageByteArray, HttpStatus.OK);
 	}
-}
 
+}
