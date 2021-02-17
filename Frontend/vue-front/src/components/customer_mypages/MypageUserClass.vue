8383bac095d23efd451716fc51cfc712ae9f997b
정예림 <jinju151002@gmail.com>
2021-02-18 03:32:29 +0900

    feat:class_url

--- Frontend/vue-front/src/components/customer_mypages/MypageUserClass.vue
+++ Frontend/vue-front/src/components/customer_mypages/MypageUserClass.vue
@@ -2,98 +2,56 @@
   <div>
     <div class="blog__sidebar__item">
       <h5>MY Classes</h5>
-
+      <v-alert
+        :value="alert"
+        color="pink"
+        dark
+        border="top"
+        icon="mdi-home"
+        transition="scale-transition"
+      >
+        수업준비중입니다.
+        <v-btn color="white" outlined @click="alert = !alert">
+          close
+        </v-btn>
+      </v-alert>
       <div
         class="blog__sidebar__recent"
         v-for="item in classDataList"
         :key="item.name"
       >
-        <router-link
-          :to="'/class/detail/' + item.classcode"
-          tag="a"
-          class="blog__sidebar__recent__item"
-        >
-          <div class="blog__sidebar__recent__item__pic">
-            <img :src="imgsrc(item.classcode)" alt="" />
-          </div>
-          <div class="blog__sidebar__recent__item__text">
-            <p>{{ item.class_name }}</p>
-            <span
-              >{{ item.class_date }} | {{ item.class_starttime.slice(0, 5) }} -
-              {{ item.class_endtime.slice(0, 5) }}</span
+        <div>
+          <router-link
+            :to="'/class/detail/' + item.classcode"
+            tag="a"
+            class="blog__sidebar__recent__item"
+          >
+            <div class="blog__sidebar__recent__item__pic">
+              <img :src="imgsrc(item.classcode)" alt="" />
+            </div>
+            <div class="blog__sidebar__recent__item__text">
+              <p>{{ item.class_name }}</p>
+            </div>
+          </router-link>
+          <div class="text-center mb-4">
+            <v-btn
+              tile
+              elevation="0"
+              style="display: inline-block;font-size: 12px;font-weight: 600;text-transform: uppercase; padding: 14px 30px;color: rgb(255, 255, 255);background: rgb(240, 134, 50);letter-spacing: 2px;width:65px;hegith:20px;margin:4px;"
+              @click="gotoClass(item.class_url)"
             >
+              class
+            </v-btn>
           </div>
-        </router-link>
+        </div>
+        <div>
+          <span
+            >{{ item.class_date }} | {{ item.class_starttime.slice(0, 5) }} -
+            {{ item.class_endtime.slice(0, 5) }}</span
+          >
+        </div>
       </div>
     </div>
-    <!-- <v-alert
-      :value="alert"
-      color="pink"
-      dark
-      border="top"
-      icon="mdi-home"
-      transition="scale-transition"
-    >
-      수업준비중입니다.
-      <v-btn color="white" outlined @click="alert = !alert">
-        close
-      </v-btn>
-    </v-alert> -->
-    <!-- <v-row no-gutters>
-      <v-col cols="12">
-        <v-simple-table fixed-header height="200px">
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-center">
-                  클래스 이름
-                </th>
-                <th class="text-center">
-                  날짜
-                </th>
-                <th class="text-center">
-                  시간
-                </th>
-                <th class="text-center">
-                  클래스 바로가기
-                </th>
-                <th class="text-center">
-                  수업입장
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in classDataList" :key="item.name">
-                <td class="text-center">{{ item.class_name }}</td>
-                <td class="text-center">{{ item.class_date }}</td>
-                <td class="text-center">
-                  {{ item.class_starttime.slice(0, 5) }}
-                </td>
-                <td>
-                  <v-btn
-                    :to="'/class/detail/' + item.classcode"
-                    class="ma-2"
-                    color="orange"
-                    style="display: flex;"
-                    >클레스 상세
-                  </v-btn>
-                </td>
-
-                <td>
-                  <div>
-                    <div class="text-center mb-4">
-                      <v-btn color="primary" @click="gotoClass(item.class_url)">
-                        Toggle
-                      </v-btn>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-col>
-    </v-row> -->
   </div>
 </template>
 
