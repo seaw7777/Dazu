240bc339fdc35a4edeee9de63a598da2a675e236
zzdhxm12@naver.com <zzdhxm12@naver.com>
2021-02-18 16:01:55 +0900

    fix

--- Frontend/vue-front/src/views/accounts/LoginSignupPage.vue
+++ Frontend/vue-front/src/views/accounts/LoginSignupPage.vue
@@ -13,14 +13,14 @@ export default {
       member: [],
     };
   },
-  async created() {
+  created() {
     this.code = this.$route.query.code;
     console.log(this.code);
     if (this.code != undefined || this.code != null) {
       this.$store.dispatch('CODE', this.code);
 
       console.log('passif--' + this.code);
-      await this.$axios
+      this.$axios
         .get('https://i4d104.p.ssafy.io/dazu/login?code=' + this.code)
         // .get('http://localhost:8000/dazu/login?code=' + this.code)
         .then(({ data }) => {
